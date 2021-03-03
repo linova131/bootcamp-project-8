e00c713007f92f62e7eb50f357d586cffce0828b
Liz Novaski <lnovaski@travelers.com>
2021-03-03 16:52:50 -0600

    Tuesday Night Update
    Update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const Book = require('../models').Book;
+
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', asyncHandler(async (req, res, next) => {
+  const books = await Book.findAll();
+  return res.json(books);
+}));
 
 module.exports = router;
