2c90bcf1addff6387abe1beed09e886c59734808
Liz Novaski <lnovaski@travelers.com>
2021-03-04 09:58:46 -0600

    Building Routes and Views
    Building out routes and views

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const {sequelize} = require('./models');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -36,18 +36,26 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  const err = new Error('Page not found');
+  err.status = 404;
+  err.message = "Sorry, that doesn't appear to be a page that exists :/ ";
+  next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  if(err.status === 404) {
+    res.locals.error = err;
+    res.render('page_not_found');
+  } else if (err.status === undefined) {
+    err.status = 500;
+    err.message = 'Something went wrong!';
+    res.locals.error = err;
+    res.render('error');
+  } else {
+    res.locals.error = err;
+    res.render('error');
+  }
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,23 @@ router.get('/', asyncHandler(async (req, res, next) => {
   return res.json(books);
 }));
 
+//Get /books, show full list of books
+router.get('/books', asyncHandler(async(req, res, next) => {
+  const books = await Book.findAll();
+  return res.json(books);
+}));
+
+//Get /books/new, shows the create new book form
+router.get('/books/new', (req, res, next) => {
+  res.render('new_book');
+});
+
+//Post /books/new, posts a new book to the database
+
+//Get /books/:id, shows book detail form
+
+//Post /books/:id, updates book info in the database
+
+//Post /books/:id/delete, deletes a book
+
 module.exports = router;
